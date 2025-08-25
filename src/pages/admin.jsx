26d68d21ba1d668e87262a326e4280e564d0ebcb
Henry chidera <chidera9713@gmail.com>
2025-08-25 21:06:52 +0100

    test

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,11 +1,12 @@
 // pages/Dashboard.jsx
-import WriterDashboard from "./writersDashboard";
-import AdminDashboard from "./adminDashboard";
+
 import BlogContext, { useBlog } from "../components/BlogContext";
 
 export default function Dashboard() {
   const { user, role } = useBlog
   if (!user) return <div className="p-10">Loading user...</div>;
 
+  return (
+    <h1>{role}</h1>
 
 }
